8f78451e82f0fbd5a2738f6085ad31da04dc629e
KristoferMar <kristofer@brostu.dk>
2020-03-01 13:42:52 +0100

    Found a bug in Contact.css file line 21.. Something went wrong there

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,41 +1,28 @@
-import React from 'react'
-import '../Contact/Contact.css';
-
+import React from 'react';
+import './Contact.css';
 
 class Contact extends React.Component {
   render() {
     return (
-<aside class="profile-card">
-  <header>
+      <aside class='profile-card'>
+        <header>
+          <a target='_blank' href='#'>
+            <img
+              alt =""
+              src='https://media-exp1.licdn.com/dms/image/C4D03AQEUh9sGr4FR4w/profile-displayphoto-shrink_200_200/0?e=1588204800&v=beta&t=VnQsgoZJRSX-5DHMgn59qbc6mtxjSrIlPS8cI-Sn9kY'
+              class='hoverZoomLink'
+            ></img>
+          </a>
 
-    <a target="_blank" href="#">
-      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEUh9sGr4FR4w/profile-displayphoto-shrink_200_200/0?e=1588204800&v=beta&t=VnQsgoZJRSX-5DHMgn59qbc6mtxjSrIlPS8cI-Sn9kY" class="hoverZoomLink"></img>
-    </a>
+          <h1>Daniel Gorgis</h1>
 
-
-    <h1>
-            Daniel Gorgis
-          </h1>
-
-    <h2>
-            Softwaree Developer/Consultant
-          </h2>
+          <h2>Softwaree Developer/Consultant</h2>
           <h5>I like to code..</h5>
-        
-    
-    
+        </header>
 
-  </header>
-
-  <div class="profile-bio">
-
- 
-
-  </div>
-
-</aside>
-
-  )
+        <div class='profile-bio'></div>
+      </aside>
+    );
   }
 }
-export default Contact
+export default Contact;
