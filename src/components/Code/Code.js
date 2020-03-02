62263e2125cd0842b4f82fbf9a8df61f930ad57e
Daniel Gorgis <daniel.gorgis-cic@ibm.com>
2020-03-02 23:05:27 +0100

    New code page

--- src/components/Code/Code.js
+++ src/components/Code/Code.js
@@ -1,30 +1,66 @@
 import React from 'react';
 import '../Code/Code.css'
 import '../Code/Code.scss'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { solarizedDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { atelierSavannaDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
 
 
-class Code extends React.Component{
+class Code extends React.Component {
 
-   enablePython(){
-       return(
-          <div></div>
-       )
-    }
 
-render(){
 
+render() {
+
+
+
+
+const codeString = `
+    
+  if plebiel == "Damen":
+
+    for i in name_list:
+        print("name of user:" + i);
+  else:
+
+  print("Manden")
+  
+ 
+  `;
+
+    let insertFields = (
+
+      <div>
+
+        <h3>Dette er et eksempel</h3>
+        <SyntaxHighlighter language="javascript" style={docco}>
+          {codeString}
+        </SyntaxHighlighter>
+
+      </div>)
 
 
     return (
-<div></div>
-      )
-        
-  
-       
-        
-}
+      <body>
+        <div style={{ overflow: 'auto' }}>
+
+          {insertFields}
+          {insertFields}
+          {insertFields}
+          {insertFields}
+
+        </div>
+
+      </body>
+
+    )
+
+
+
+
+  }
 }
 export { Code as default };
