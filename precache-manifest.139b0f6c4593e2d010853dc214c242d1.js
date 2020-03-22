ab31ab9869173fc57a7c616cfd3e3a4f79083177
Daniel Gorgis <daniel.gorgis-cic@ibm.com>
2020-03-23 00:55:47 +0100

    Updates

---
+++ precache-manifest.139b0f6c4593e2d010853dc214c242d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cef68da0906aeb1e3e08c4c78c1689",
+    "url": "/react-homepage-daniel/index.html"
+  },
+  {
+    "revision": "79228de7b41de5c80695",
+    "url": "/react-homepage-daniel/static/css/main.b6697f3e.chunk.css"
+  },
+  {
+    "revision": "85f6fe52c6d43eb57706",
+    "url": "/react-homepage-daniel/static/js/2.e56bef96.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-homepage-daniel/static/js/2.e56bef96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79228de7b41de5c80695",
+    "url": "/react-homepage-daniel/static/js/main.73b3dca8.chunk.js"
+  },
+  {
+    "revision": "38f9a8aa39e47e0393d7",
+    "url": "/react-homepage-daniel/static/js/runtime-main.f08262f2.js"
+  }
+]);
